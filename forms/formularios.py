4cf667d3b7154cb7e31daf8963d161cb29f1ed0e
Leo Vassallo <vassallol@uninorte.edu.co>
2021-10-14 10:26:51 -0500

    Añadí el Check al formulario de Registro

--- forms/formularios.py
+++ forms/formularios.py
@@ -1,12 +1,12 @@
 from flask.app import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired
 
 class Login(FlaskForm):
     username = StringField("Usuario", validators=[DataRequired("Usuario es obligatorio")])
     password = PasswordField("Password", validators=[DataRequired("Password es obligatorio")])
-    enviar = SubmitField ("Iniciar sesion") 
+    enviar = SubmitField ("Enviar")
     
 class Registro(FlaskForm):
     username = StringField("Usuario")
@@ -15,3 +15,4 @@ class Registro(FlaskForm):
     password = PasswordField("Password")
     registrar = SubmitField("Registrar")
     rol = SelectField("Rol", coerce=int, choices=[("0","-- Elija opción -- "),("1","Piloto"),("2","Usuario")], validators=[DataRequired(message="Debe escoger una opción")])
+    check = BooleanField('Acepto términos y condiciones', validators=[DataRequired()])
